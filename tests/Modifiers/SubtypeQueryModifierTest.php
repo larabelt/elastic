ba4c9523f488cd2db315e88681a43171ea38dcc7
Matt Crowe <github.mpc@zym.me>
2018-07-20 16:28:41 +0000

    continue refactor template to subtype

---
+++ tests/Modifiers/SubtypeQueryModifierTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Mockery as m;
+use Belt\Core\Http\Requests\PaginateRequest;
+use Belt\Core\Testing\BeltTestCase;
+use Belt\Elastic\Engine as ElasticEngine;
+use Belt\Elastic\Modifiers\SubtypeQueryModifier;
+
+class SubtypeQueryModifierTest extends BeltTestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    /**
+     * @covers \Belt\Elastic\Modifiers\SubtypeQueryModifier::modify
+     */
+    public function test()
+    {
+        $engine = m::mock(ElasticEngine::class);
+        $modifier = new SubtypeQueryModifier($engine);
+
+        $this->assertFalse(isset($engine->query['bool']['must'][0]['terms']['subtype']));
+        $modifier->modify(new PaginateRequest(['subtype' => 'foo']));
+        $this->assertTrue(isset($engine->query['bool']['must'][0]['terms']['subtype']));
+    }
+
+}
