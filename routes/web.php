7f972f18a60ff05de85b24533681c901f333ff7d
Matt Crowe <github.mpc@zym.me>
2018-12-05 16:33:06 +0000

    continue translations

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,13 @@ use Belt\Content\Http\Controllers\Web;
 
 Route::group(['middleware' => ['web']], function () {
 
-    # search
+    if (config('belt.core.translate.prefix-urls')) {
+        foreach ((array) config('belt.core.translate.locales') as $locale) {
+            $code = array_get($locale, 'code');
+            Route::get("$code/search", Web\SearchController::class . '@index');
+        }
+    }
+
     Route::get('search', Web\SearchController::class . '@index');
 
 });
